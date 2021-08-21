d598f5cc0e6eed6798c0224c8a055ebfae3ba6ed
bobycat22 <anthonyznj@gmail.com>
2021-08-20 22:13:17 -0400

    update django errors

--- django_peddiehacks/core/urls.py
+++ django_peddiehacks/core/urls.py
@@ -4,6 +4,9 @@ from core import views
 
 urlpatterns = [
     path('', views.index),
-	path('get-dummy-model/', views.DummyModelList.as_view()),
-    path('create-dummy-model/', views.createDummyModel),
+	path('get-reports/', views.ReportList.as_view()),
+	path('get-report-types/', views.ReportTypeList.as_view()),
+	path('get-alerts/', views.AlertList.as_view()),
+    path('create-report/', views.createReport),
+    path('create-alert/', views.createAlert),
 ]
